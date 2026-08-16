90c07824a9fcd8ec3eccb21937ab52944814bfd3
OKD-Programmer <ProgrammerOKD@gmail.com>
2026-08-16 20:41:40 +0300

    Enhance course introduction with Python overview
    Add introductory content about Python in English and Arabic.

---
+++ Intruduction_to_Pythoncourse.py
@@ -0,0 +1,19 @@
+#  Python
+#  مقدمة عن تعريف الكورس الذي سنتحدث عنه (لغة البايثون)
+#  An introduction to the course we will be discussing (the Python language).
+name = 'OKD Programmer'
+lesson = 1
+print('Welcome to:',name,'Lesson number: ',lesson)
+IntroENG = '''
+Python is a high-level, easy-to-learn programming language known for its simple and readable syntax.
+Main Uses: Web development, Data Science, Artificial Intelligence (AI), and Task Automation.
+Importance: It is the top choice for beginners due to its simplicity, and essential for pros due to its massive library ecosystem and productivity.
+'''
+
+IntroARB = '''
+ (Python) تُعد بايثون
+ .لغة برمجة عالية المستوى وسهلة التعلم، وتتميز ببنية برمجية بسيطة وواضحة
+الاستخدامات الرئيسية: تطوير الويب، وعلم البيانات، والذكاء الاصطناعي، وأتمتة المهام 
+الأهمية: تُعتبر الخيار الأول للمبتدئين نظراً لبساطتها، كما أنها ضرورية للمحترفين بفضل منظومتها الضخمة من المكتبات البرمجية وقدرتها العالية على تعزيز الإنتاجية
+'''
+print(IntroENG,'\n',IntroARB)
--- Intruduction_to_variables.py
+++
@@ -1,5 +0,0 @@
-Python
-مقدمة عن المتغيرات الأساسية في لغة بايثون
-name = 'OKD Programmer'
-lesson = 1
-print('Welcome to:',name,'Lesson number: ',lesson)
